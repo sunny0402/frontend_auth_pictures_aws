5f586a22c2529e01afc86a6795694ca4f246a6f1
sunny0402 <sunnycodes0402@gmail.com>
2022-09-24 00:59:10 -0400

    Test deployment frontend.

---
+++ src/api/axios.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:5000";
+
+//testing locally backend will run on 5000
+export default axios.create({
+  baseURL: BASE_URL,
+});
+
+//to refresh tokens in background
+export const axiosPrivate = axios.create({
+  baseURL: BASE_URL,
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true,
+});
---
+++ src/components/Logout.js
@@ -0,0 +1,75 @@
+import { useRef, useState, useEffect } from "react";
+import useAuth from "../hooks/useAuth";
+// import { useNavigate } from "react-router-dom";
+
+import axios from "../api/axios";
+const LOGOUT_URL = "/logout";
+
+const Logout = () => {
+  const { auth, setAuth } = useAuth();
+  const errRef = useRef();
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+  // const navigate = useNavigate();
+
+  const handleLogout = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.get(LOGOUT_URL, {
+        // headers: { "Content-Type": "application/json" },
+        withCredentials: true,
+      });
+      console.log(JSON.stringify(response));
+
+      //!!!!
+      //TODO: add a logout method to useAuth hook which clears data
+      //TODO: refresh token not delted in database after logout....
+      // TODO: user page looses data after refresh
+      // currently secure: true so only https ... test in production
+      setAuth((prev) => {
+        return { ...prev, user: "", roles: "", accessToken: "" };
+      });
+
+      console.log(
+        ">>> DEBUG: Logout.js: auth?.user, auth?.roles, auth?.accessToken: ",
+        auth?.user,
+        auth?.roles,
+        auth?.accessToken
+      );
+
+      // Note: After successful logout show success message
+      setSuccess(true);
+    } catch (err) {
+      setErrMsg("Error Logging Out.");
+      console.log(`Error logging out: ${err}`);
+    }
+  };
+
+  return (
+    <>
+      {success ? (
+        <section>
+          <h1>Success! You've logged out.</h1>
+          <p>
+            <br />
+            <a href="login">Sign In</a>
+            <br />
+            <a href="/">Home</a>
+          </p>
+        </section>
+      ) : (
+        <section>
+          <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"}>
+            {errMsg}
+          </p>
+          <h1>Sign Out</h1>
+          <form onSubmit={handleLogout}>
+            <button>Sign Out</button>
+          </form>
+        </section>
+      )}
+    </>
+  );
+};
+
+export default Logout;
