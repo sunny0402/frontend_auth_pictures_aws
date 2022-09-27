2a21f05f7373ab630297451494d0a617a4ee0ba7
sunny0402 <sunnycodes0402@gmail.com>
2022-09-27 06:47:30 -0400

    test ...

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api";
+// const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "";
 
 //testing locally backend will run on 5000
 export default axios.create({
